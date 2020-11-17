4bb92b1f75ee847b72ca9cbb6a0c2a4afe350c46
Darzwaitz <darzwaitz@yahoo.com>
2020-11-17 12:55:44 +0000

    initial files added

---
+++ main.js
@@ -0,0 +1,9 @@
+//alert('sup');
+let app = new Vue({
+    el: '#app',
+    data: {
+        product: 'Sox',
+        description: 'This iz the description',
+        image: 'https://images.pexels.com/photos/251454/pexels-photo-251454.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+    }
+})
