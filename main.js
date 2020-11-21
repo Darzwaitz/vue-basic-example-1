1418b1ba5d606a57f5c2e0617739f350489d976c
Darzwaitz <darzwaitz@yahoo.com>
2020-11-21 17:05:33 +0000

    added product info on the page using binding

--- main.js
+++ main.js
@@ -4,6 +4,19 @@ let app = new Vue({
     data: {
         product: 'Sox',
         description: 'This iz the description',
-        image: 'https://images.pexels.com/photos/251454/pexels-photo-251454.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+        image: 'https://images.pexels.com/photos/251454/pexels-photo-251454.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+        inStock: true,
+        inventory: 2,
+        details: ["80 cotton","20% polyester","100% comfy!"],
+        variants: [
+            {
+                variantId: 01,
+                variantColour: "Green"
+            },
+            {
+                variantId: 02,
+                variantColour: "Blue"
+            }
+        ]
     }
 })
