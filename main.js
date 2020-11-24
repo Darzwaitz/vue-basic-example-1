fa82468e0be0706d51e0264c5f4b55b17cf56771
Darzwaitz <darzwaitz@yahoo.com>
2020-11-24 12:48:33 +0000

    pics change on colour hover - need to fix cart disable when empty

--- main.js
+++ main.js
@@ -5,18 +5,39 @@ let app = new Vue({
         product: 'Sox',
         description: 'This iz the description',
         image: 'https://images.pexels.com/photos/251454/pexels-photo-251454.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
-        inStock: true,
-        inventory: 2,
+         inventory: 0,
+         inStock: true,
+       
         details: ["80 cotton","20% polyester","100% comfy!"],
         variants: [
             {
                 variantId: 01,
-                variantColour: "Green"
+                variantColour: "Burlywood",
+                variantImage: 'https://images.pexels.com/photos/251454/pexels-photo-251454.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
             },
             {
                 variantId: 02,
-                variantColour: "Blue"
+                variantColour: "Fuchsia",
+                variantImage: 'https://images.pexels.com/photos/1117485/pexels-photo-1117485.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
             }
-        ]
+        ],
+        cart: 0
+    },
+    methods: {
+        addToCart: function(){
+            this.cart +=1
+        },
+        zeroCart: function(){
+            this.cart = 0
+        },
+        //ES6 function
+        updateProduct(variantImage){
+            this.image = variantImage
+        },
+        inStockTrigger(inStock) {
+            if(inventory <= 0)
+            {inStock = false;}
+          }            
     }
+      
 })
